da6b5a23b4c8fb0967e1b5b064a382f68f15c9cf
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-01 10:05:21 +0600

    add function and complet

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const container = document.getElementById("container");
-const text = document.getElementById("text");
+ document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("container");
+  const text = document.getElementById("text");
 
-const totalTime = 7500;
-const breathTime = (totalTime / 5) * 2;
-const holdTime = totalTime / 5;
+  const totalTime = 7500;
+  const breathTime = (totalTime / 5) * 2;
+  const holdTime = totalTime / 5;
 
-brethAnimation();
+  breathAnimation();
 
-function brethAnimation() {
-  text.innerText = "Breath In!";
-
-  setTimeout(() => {
-    text.innerText = "Hold";
+  function breathAnimation() {
+    text.innerText = "Breathe In!";
+    container.className = "container grow";
 
     setTimeout(() => {
-      text.innerText = "Breathe Out!";
-    }, holdTime);
-  }, breathTime);
-}
+      text.innerText = "Hold";
+
+      setTimeout(() => {
+        text.innerText = "Breathe Out!";
+        container.className = "container shrink";
+      }, holdTime);
+    }, breathTime);
+  }
+
+  setInterval(breathAnimation, totalTime);
+});
